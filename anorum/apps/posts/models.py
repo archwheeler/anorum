5744d55de44baf4f728140617e2ec2322148881a
Archie Wheeler <archiewheeler@pm.me>
2022-03-20 18:19:42 +0000

    Add created_at field to Post model

--- anorum/apps/posts/models.py
+++ anorum/apps/posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
