e0c2934d432232cf4682e48807eac4170ab44f35
Archie Wheeler <archiewheeler@pm.me>
2022-03-20 18:48:05 +0000

    Create template for posts index view

--- anorum/apps/posts/views.py
+++ anorum/apps/posts/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 
+from django.template import loader
+
+from .models import Post
+
 # Create your views here.
 def index(request):
-    return HttpResponse("posts")
+    latest_posts_list = Post.objects.order_by("-created_at")[:5]
+    template = loader.get_template("posts/index.html")
+    context = {
+        "latest_posts_list": latest_posts_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def post(request, post_id):
