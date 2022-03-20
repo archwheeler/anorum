6eefd3291d2293af444aae55110a4613f6170827
Archie Wheeler <archiewheeler@pm.me>
2022-03-20 18:09:45 +0000

    Create basic post view

--- anorum/apps/posts/views.py
+++ anorum/apps/posts/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("posts")
+
+
+def post(request, post_id):
+    response = "post %s"
+    return HttpResponse(response % post_id)
