d455ff3c5f4d30f663d72b0d10d577585b1d6ef6
Archie Wheeler <archiewheeler@pm.me>
2022-03-23 19:25:56 +0000

    Add 'posts' namespace to posts app URLs

--- anorum/apps/posts/urls.py
+++ anorum/apps/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "posts"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:post_id>/", views.post, name="post"),
