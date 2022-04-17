f9e63368941ae3cf379d806f39e96d89a0278ffb
Archie Wheeler <archiewheeler@pm.me>
2022-04-17 14:50:36 +0100

    Delete repository

--- anorum/apps/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "posts"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:post_id>/", views.post, name="post"),
-]
