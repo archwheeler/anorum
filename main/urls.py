2a8dd79e2fe884748ff9fe687dbc2dce2b0de6a8
Archie Wheeler <archiewheeler@pm.me>
2022-04-17 18:54:48 +0100

    Replace auth include with individual auth views

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
-from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 from main.views import IndexView
 
 urlpatterns = [
+    # anorum
     path("", IndexView.as_view(), name="index"),
+    # accounts
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
