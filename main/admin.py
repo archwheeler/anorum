d17895342e49adb8a8d0bf20ee90bc67028e4947
Archie Wheeler <archiewheeler@pm.me>
2022-04-18 18:03:53 +0100

    Register Post model with admin site

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from main.models import Forum, User
+from main.models import Forum, Post, User
 
 # Register your models here.
 admin.site.register(Forum)
+admin.site.register(Post)
 admin.site.register(User)
