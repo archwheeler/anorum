23d0138feb3814f7d8cc92c9a6b263b107a1beca
Archie Wheeler <archiewheeler@pm.me>
2022-05-15 11:36:32 +0100

    Create production settings

---
+++ anorum/production_settings.py
@@ -0,0 +1,37 @@
+"""
+Test Django settings for anorum project.
+"""
+
+import os
+
+from anorum.shared_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    os.environ['ALLOWED_HOSTS'].split()
+]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRESQL_DBNAME'],
+        'USER': os.environ['POSTGRESQL_USER'],
+        'PASSWORD': os.environ['POSTGRESQL_PASSWORD'],
+        'HOST': os.environ['POSTGRESQL_HOST'],
+        'PORT': os.environ['POSTGRESQL_PORT'],
+    }
+}
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
