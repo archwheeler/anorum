2d81e9b2816fb79f0fa91f18c3a0c5d7e2ab849d
Archie Wheeler <archiewheeler@pm.me>
2022-06-06 21:00:12 +0100

    Remove inline javascript

--- main/static/script.js
+++ main/static/script.js
@@ -1,14 +1,20 @@
-function onBodyLoad() {
-    // Turn mentions into links
-    regexForMentions = /(\B@(\d+)\b)/g;
-    for (postContent of document.getElementsByClassName("post-content")) {
+document.addEventListener('DOMContentLoaded', function () {
+    const regexForMentions = /(\B@(\d+)\b)/g;
+
+    for (post of document.getElementsByClassName("post-container")) {
+        // Turn mentions into links
+        postContent = post.getElementsByClassName("post-content")[0];
         element = postContent.getElementsByTagName("p")[0];
         element.innerHTML = element.innerHTML.replace(
             regexForMentions,
             "<a href=https://anorum.com/mention/$2>$1</a>"
         );
+
+        // Add reply button functionality
+        replyButton = post.getElementsByClassName("post-reply")[0];
+        replyButton.addEventListener('click', _ => replyToPost(post.id));
     }
-}
+});
 
 function replyToPost(post_id) {
     replyBox = document.getElementsByTagName("textarea")[0];
